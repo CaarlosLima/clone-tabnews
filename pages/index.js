0dd33273cb8089e4fa079c6abe13b2a465416ef3
Carlos Eduardo de Lima <caduu@live.com>
2024-09-25 16:44:23 -0300

    hawkudo habilitado

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <h1>Salve salve</h1>;
+  return (
+    <div className="page-container">
+      <div className="frame">
+        <div className="hawk-container">
+          <h1>O HOMI TA HABILITADO PORRA!!</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
